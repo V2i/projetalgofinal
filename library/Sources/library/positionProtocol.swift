1660f42e2e6887c8bfac5f5d2fc934a3eecd8c58
V2i <43175508+V2i@users.noreply.github.com>
2019-01-13 19:43:48 +0100

    comment position 2

--- library/Sources/library/positionProtocol.swift
+++ library/Sources/library/positionProtocol.swift
@@ -25,8 +25,13 @@ public protocol PositionProtocol{
     //post : renvoie True s'il n'ya pas de carte sur la position donnée, False sinon
 	func estPositionVide()->Bool
 
-	// Fonction pour avoir la carte sur la position donnée
+	//Fonction pour avoir la carte sur la position donnée
     //getCarte : PositionStruct -> (CarteStruct | Vide)
-    // post :renvoie vide s'il n'y a pas de carte sur la position donnée, sinon renvoie la Carte 
+    //post :renvoie vide s'il n'y a pas de carte sur la position donnée, sinon renvoie la Carte 
 	func getCarte()->CarteStruct?
+
+	//Fonction pour savoir a qui appartient la carte
+    //getCarteAdverse : PositionStruct -> Bool
+    //post : renvoie true si la carte appartient au joueur adverse, false sinon
+	func getCarteAdverse() -> Bool
 }
