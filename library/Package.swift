2de23f5dcb6d2dec55c5fd346aa368979a3b1d48
V2i <43175508+V2i@users.noreply.github.com>
2019-01-14 12:31:14 +0100

    gg

--- library/Package.swift
+++ library/Package.swift
@@ -8,17 +8,15 @@ let package = Package(
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(name: "library",targets: ["library"]),
-	.executable(name: "main", targets:["main"]),
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
-        .package(path:"library"),
+        //.package(path:"library"),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
 	.target(
-	    name: "main", dependencies:["main"]),
         .target(
             name: "library",
             dependencies: []),
